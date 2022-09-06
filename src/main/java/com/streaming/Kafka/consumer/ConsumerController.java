be6e9b070837e5335945a3600b96eea37060f017
Reja <rezanurrochmat@gmail.com>
2022-09-06 11:27:56 +0700

    add consumer controller

--- src/main/java/com/streaming/Kafka/consumer/ConsumerController.java
+++ src/main/java/com/streaming/Kafka/consumer/ConsumerController.java
@@ -6,15 +6,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @Slf4j
 public class ConsumerController {
-    Logger logger = LoggerFactory.getLogger(ProducerController.class);
+    Logger logger = LoggerFactory.getLogger(ConsumerController.class);
+
     @Value("${spring.kafka.user.topic}")
     private String userTopic;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String kafkaBrokerGroupId;
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
+
+    @KafkaListener(topics = "user", groupId = "myGroup")
+    public void userStreamingListener(String msg) {
+        logger.info("Streaming message from user topic {} : ", msg);
+    }
+
 
 
 }
