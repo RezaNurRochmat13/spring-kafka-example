f91128b4a1780854ce5f74aab0df4ebf1f75c315
Reja <rezanurrochmat@gmail.com>
2022-09-07 08:32:17 +0700

    remove unused code

--- src/main/java/com/streaming/Kafka/consumer/ConsumerController.java
+++ src/main/java/com/streaming/Kafka/consumer/ConsumerController.java
@@ -1,11 +1,9 @@
 package com.streaming.Kafka.consumer;
 
-import com.streaming.Kafka.producer.ProducerController;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Controller;
